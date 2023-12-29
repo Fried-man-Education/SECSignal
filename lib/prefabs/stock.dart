accfc51c82076a2fc8e28b36b019ae2aeee0b497
Andrew Friedman <andrewfriedman01@gmail.com>
2023-12-28 23:27:06 -0500

    Fix bug with stock chart parsing null data

--- lib/prefabs/stock.dart
+++ lib/prefabs/stock.dart
@@ -180,8 +180,34 @@ class _StockGraphCardState extends State<StockGraphCard> {
 
   List<FlSpot> _getSpots(Map<String, dynamic> data) {
     List<int> timestamps = List<int>.from(data["timestamp"]);
-    List<double> openPrices =
-        List<double>.from(data["indicators"]["quote"].first["open"]);
+
+    List<dynamic> rawData = data["indicators"]["quote"].first["open"] as List;
+    List<double> openPrices = [];
+
+    for (int i = 0; i < rawData.length; i++) {
+      var currentValue = rawData[i];
+      double? parsedValue = double.tryParse(currentValue.toString());
+
+      if (parsedValue != null) {
+        // If the current value is parsable, add it to the list.
+        openPrices.add(parsedValue);
+      } else {
+        // If the current value is not parsable, try to use the previous or next valid value.
+        double? replacementValue;
+        if (i > 0 && double.tryParse(rawData[i - 1].toString()) != null) {
+          // Use the previous value if available and valid.
+          replacementValue = double.parse(rawData[i - 1].toString());
+        } else if (i < rawData.length - 1 && double.tryParse(rawData[i + 1].toString()) != null) {
+          // If the previous value isn't available or valid, try the next value.
+          replacementValue = double.parse(rawData[i + 1].toString());
+        }
+
+        // If a replacement was found and it's not 0, add it to the list.
+        if (replacementValue != null && replacementValue != 0) {
+          openPrices.add(replacementValue);
+        }
+      }
+    }
 
     // Determine the cutoff timestamp for one year ago
     int oneYearAgoTimestamp = DateTime.now()
