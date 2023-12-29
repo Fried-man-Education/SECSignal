b2666a12535967401b1c3d367aa38c29dc680f8a
Andrew Friedman <andrewfriedman01@gmail.com>
2023-12-29 01:21:15 -0500

    Fix company loading prefab bug

--- lib/prefabs/company.dart
+++ lib/prefabs/company.dart
@@ -77,16 +77,23 @@ class _CompanySectionState extends State<CompanySection> {
         if (snapshot.hasError) return Container();
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          content.add(SizedBox(
-              height: 500,
-              child: Center(child: PlatformCircularProgressIndicator(
-                material: (_, __) =>
-                    MaterialProgressIndicatorData(
-                      valueColor:
-                      AlwaysStoppedAnimation<Color>(
-                          Theme.of(context).primaryColor),
-                    ),
-              ))));
+          return Column(
+            children: [
+              content[0],
+              if (content.length == 2)
+                content[1],
+              SizedBox(
+                  height: 500,
+                  child: Center(child: PlatformCircularProgressIndicator(
+                    material: (_, __) =>
+                        MaterialProgressIndicatorData(
+                          valueColor:
+                          AlwaysStoppedAnimation<Color>(
+                              Theme.of(context).primaryColor),
+                        ),
+                  ))),
+            ],
+          );
         }else if (!snapshot.hasData || snapshot.data!.isEmpty) {
           return Container();
         }else {
